3a3c3397a006b9a2a2381291719dd9ab77049795
perfluka8760 <canalluka8760@gmail.com>
2023-06-06 15:37:18 -0300

    Initial commit

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/teste', function(){
+    return ['msg'=> 'ola mundo'];
+});
+
+
+Route::get('/products', ['App\Http\Controllers\Api\ProductController', 'index']);
+Route::post('/products', ['App\Http\Controllers\Api\ProductController', 'save']);
+Route::get('/products/{id}', ['App\Http\Controllers\Api\ProductController', 'show']);
+Route::put('/products', ['App\Http\Controllers\Api\ProductController', 'update']);
+
+
+
+
+
+
